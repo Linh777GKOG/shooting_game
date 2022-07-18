a2de61bbd2cc0503f8bd033db12730458a8f7e49
Linh Vu <linh777gkog@gmail.com>
2022-07-18 21:14:26 +0700

    creat smoke explosion

--- script.js
+++ script.js
@@ -454,3 +454,9 @@ this.image,
     );
   }
 }
+ class SmokeExplosion extends Explosion {
+        constructor(game, x, y){
+            super(game, x, y);
+            this.image = document.getElementById('smokeExplosion');
+        }
+    }
