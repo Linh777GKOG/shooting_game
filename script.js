24fee2331c3a00a41257b4f77c93dc6772c964e5
Linh Vu <linh777gkog@gmail.com>
2022-07-18 21:12:59 +0700

    craet background

--- script.js
+++ script.js
@@ -382,3 +382,17 @@ class Layer {
     context.drawImage(this.image, this.x + this.width, this.y);
   }
 }
+
+class Background {
+        constructor(game){
+            this.game = game;
+            this.image1 = document.getElementById('layer1');
+            this.image2 = document.getElementById('layer2');
+            this.image3 = document.getElementById('layer3');
+            this.image4 = document.getElementById('layer4');
+            this.layer1 = new Layer(this.game, this.image1, 0.2);
+            this.layer2 = new Layer(this.game, this.image2, 0.4);
+            this.layer3 = new Layer(this.game, this.image3, 1);
+            this.layer4 = new Layer(this.game, this.image4, 1.5);
+            this.layers = [this.layer1, this.layer2, this.layer3];
+        }
