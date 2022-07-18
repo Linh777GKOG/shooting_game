4be63795655ea476dde682ffd36e617a787bc99c
Linh Vu <linh777gkog@gmail.com>
2022-07-18 21:02:58 +0700

    make sheld

--- script.js
+++ script.js
@@ -1,62 +1,104 @@
-window.addEventListener('load', function(){
-    // canvas setup
-    const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
-    canvas.width = 700;
-    canvas.height = 500;
+window.addEventListener('load', function () {
+  // canvas setup
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 700;
+  canvas.height = 500;
 
-    class InputHandler {
-        constructor(game){
-            this.game = game;
-            window.addEventListener('keydown', e => {
-                if ((   (e.key === 'ArrowUp') ||
-                        (e.key === 'ArrowDown')
-                ) && this.game.keys.indexOf(e.key) === -1){
-                    this.game.keys.push(e.key);
-                } else if ( e.key === ' '){
-                    this.game.player.shootTop();
-                } else if ( e.key === 'd'){
-                    this.game.debug = !this.game.debug;
-                }
-            });
-            window.addEventListener('keyup', e =>{
-                if (this.game.keys.indexOf(e.key) > -1){
-                    this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
-                }
-            });
+  class InputHandler {
+    constructor(game) {
+      this.game = game;
+      window.addEventListener('keydown', (e) => {
+        if (
+          (e.key === 'ArrowUp' || e.key === 'ArrowDown') &&
+          this.game.keys.indexOf(e.key) === -1
+        ) {
+          this.game.keys.push(e.key);
+        } else if (e.key === ' ') {
+          this.game.player.shootTop();
+        } else if (e.key === 'd') {
+          this.game.debug = !this.game.debug;
         }
+      });
+      window.addEventListener('keyup', (e) => {
+        if (this.game.keys.indexOf(e.key) > -1) {
+          this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+        }
+      });
     }
-    class SoundController {
-        constructor(){
-            this.powerUpSound = document.getElementById('powerup');
-            this.powerDownSound = document.getElementById('powerdown');
-            this.explosionSound = document.getElementById('explosion');
-            this.shotSound = document.getElementById('shot');
-            this.hitSound = document.getElementById('hit');
-            this.shieldSound = document.getElementById('shieldSound');
+  }
+  class SoundController {
+    constructor() {
+      this.powerUpSound = document.getElementById('powerup');
+      this.powerDownSound = document.getElementById('powerdown');
+      this.explosionSound = document.getElementById('explosion');
+      this.shotSound = document.getElementById('shot');
+      this.hitSound = document.getElementById('hit');
+      this.shieldSound = document.getElementById('shieldSound');
+    }
+    powerUp() {
+      this.powerUpSound.currentTime = 0;
+      this.powerUpSound.play();
+    }
+    powerDown() {
+      this.powerDownSound.currentTime = 0;
+      this.powerDownSound.play();
+    }
+    explosion() {
+      this.explosionSound.currentTime = 0;
+      this.explosionSound.play();
+    }
+    shot() {
+      this.shotSound.currentTime = 0;
+      this.shotSound.play();
+    }
+    hit() {
+      this.hitSound.currentTime = 0;
+      this.hitSound.play();
+    }
+    shield() {
+      this.shieldSound.currentTime = 0;
+      this.shieldSound.play();
+    }
+  }
+  class Shield {
+    constructor(game) {
+      this.game = game;
+      this.width = this.game.player.width;
+      this.height = this.game.player.height;
+      this.frameX = 0;
+      this.maxFrame = 24;
+      this.image = document.getElementById('shield');
+      this.fps = 60;
+      this.timer = 0;
+      this.interval = 1000 / this.fps;
+    }
+    update(deltaTime) {
+      if (this.frameX <= this.maxFrame) {
+        if (this.timer > this.interval) {
+          this.frameX++;
+          this.timer = 0;
+        } else {
+          this.timer += deltaTime;
         }
-        powerUp(){
-            this.powerUpSound.currentTime = 0;
-            this.powerUpSound.play();
-        }
-        powerDown(){
-            this.powerDownSound.currentTime = 0;
-            this.powerDownSound.play();
-        }
-        explosion(){
-            this.explosionSound.currentTime = 0;
-            this.explosionSound.play();
-        }
-        shot(){
-            this.shotSound.currentTime = 0;
-            this.shotSound.play();
-        }
-        hit(){
-            this.hitSound.currentTime = 0;
-            this.hitSound.play();
-        }
-        shield(){
-            this.shieldSound.currentTime = 0;
-            this.shieldSound.play();
-        }
-    })
+      }
+    }
+    draw(context) {
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        0,
+        this.width,
+        this.height,
+        this.game.player.x,
+        this.game.player.y,
+        this.width,
+        this.height
+      );
+    }
+    reset() {
+      this.frameX = 0;
+      this.game.sound.shield();
+    }
+  }
+});
