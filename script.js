72f6591d294c29a5a096228657adf369a119ba7d
Linh Vu <linh777gkog@gmail.com>
2022-07-18 21:12:21 +0700

    lucky fish

--- script.js
+++ script.js
@@ -334,3 +334,17 @@ window.addEventListener('load', function () {
     }
   }
 });
+
+class LuckyFish extends Enemy {
+  constructor(game) {
+    super(game);
+    this.width = 99;
+    this.height = 95;
+    this.y = Math.random() * (this.game.height * 0.95 - this.height);
+    this.image = document.getElementById('lucky');
+    this.frameY = Math.floor(Math.random() * 2);
+    this.lives = 5;
+    this.score = 15;
+    this.type = 'lucky';
+  }
+}
