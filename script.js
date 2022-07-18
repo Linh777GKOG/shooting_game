ed89e04eb8e600019973d776d4dfe5fd1443a100
Linh Vu <linh777gkog@gmail.com>
2022-07-18 21:04:06 +0700

    contructor particle

--- script.js
+++ script.js
@@ -1,123 +1,132 @@
-window.addEventListener('load', function () {
-  // canvas setup
-  const canvas = document.getElementById('canvas1');
-  const ctx = canvas.getContext('2d');
-  canvas.width = 700;
-  canvas.height = 500;
+window.addEventListener('load', function(){
+    // canvas setup
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 700;
+    canvas.height = 500;
 
-  class InputHandler {
-    constructor(game) {
-      this.game = game;
-      window.addEventListener('keydown', (e) => {
-        if (
-          (e.key === 'ArrowUp' || e.key === 'ArrowDown') &&
-          this.game.keys.indexOf(e.key) === -1
-        ) {
-          this.game.keys.push(e.key);
-        } else if (e.key === ' ') {
-          this.game.player.shootTop();
-        } else if (e.key === 'd') {
-          this.game.debug = !this.game.debug;
+    class InputHandler {
+        constructor(game){
+            this.game = game;
+            window.addEventListener('keydown', e => {
+                if ((   (e.key === 'ArrowUp') ||
+                        (e.key === 'ArrowDown')
+                ) && this.game.keys.indexOf(e.key) === -1){
+                    this.game.keys.push(e.key);
+                } else if ( e.key === ' '){
+                    this.game.player.shootTop();
+                } else if ( e.key === 'd'){
+                    this.game.debug = !this.game.debug;
+                }
+            });
+            window.addEventListener('keyup', e =>{
+                if (this.game.keys.indexOf(e.key) > -1){
+                    this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+                }
+            });
         }
-      });
-      window.addEventListener('keyup', (e) => {
-        if (this.game.keys.indexOf(e.key) > -1) {
-          this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+    }
+    class SoundController {
+        constructor(){
+            this.powerUpSound = document.getElementById('powerup');
+            this.powerDownSound = document.getElementById('powerdown');
+            this.explosionSound = document.getElementById('explosion');
+            this.shotSound = document.getElementById('shot');
+            this.hitSound = document.getElementById('hit');
+            this.shieldSound = document.getElementById('shieldSound');
         }
-      });
-    }
-  }
-  class SoundController {
-    constructor() {
-      this.powerUpSound = document.getElementById('powerup');
-      this.powerDownSound = document.getElementById('powerdown');
-      this.explosionSound = document.getElementById('explosion');
-      this.shotSound = document.getElementById('shot');
-      this.hitSound = document.getElementById('hit');
-      this.shieldSound = document.getElementById('shieldSound');
-    }
-    powerUp() {
-      this.powerUpSound.currentTime = 0;
-      this.powerUpSound.play();
-    }
-    powerDown() {
-      this.powerDownSound.currentTime = 0;
-      this.powerDownSound.play();
-    }
-    explosion() {
-      this.explosionSound.currentTime = 0;
-      this.explosionSound.play();
-    }
-    shot() {
-      this.shotSound.currentTime = 0;
-      this.shotSound.play();
-    }
-    hit() {
-      this.hitSound.currentTime = 0;
-      this.hitSound.play();
-    }
-    shield() {
-      this.shieldSound.currentTime = 0;
-      this.shieldSound.play();
-    }
-  }
-  class Shield {
-    constructor(game) {
-      this.game = game;
-      this.width = this.game.player.width;
-      this.height = this.game.player.height;
-      this.frameX = 0;
-      this.maxFrame = 24;
-      this.image = document.getElementById('shield');
-      this.fps = 60;
-      this.timer = 0;
-      this.interval = 1000 / this.fps;
-    }
-    update(deltaTime) {
-      if (this.frameX <= this.maxFrame) {
-        if (this.timer > this.interval) {
-          this.frameX++;
-          this.timer = 0;
-        } else {
-          this.timer += deltaTime;
+        powerUp(){
+            this.powerUpSound.currentTime = 0;
+            this.powerUpSound.play();
         }
-      }
+        powerDown(){
+            this.powerDownSound.currentTime = 0;
+            this.powerDownSound.play();
+        }
+        explosion(){
+            this.explosionSound.currentTime = 0;
+            this.explosionSound.play();
+        }
+        shot(){
+            this.shotSound.currentTime = 0;
+            this.shotSound.play();
+        }
+        hit(){
+            this.hitSound.currentTime = 0;
+            this.hitSound.play();
+        }
+        shield(){
+            this.shieldSound.currentTime = 0;
+            this.shieldSound.play();
+        }
     }
-    draw(context) {
-      context.drawImage(
-        this.image,
-        this.frameX * this.width,
-        0,
-        this.width,
-        this.height,
-        this.game.player.x,
-        this.game.player.y,
-        this.width,
-        this.height
-      );
+    class Shield {
+        constructor(game){
+            this.game = game;
+            this.width = this.game.player.width;
+            this.height = this.game.player.height;
+            this.frameX = 0;
+            this.maxFrame = 24;
+            this.image = document.getElementById('shield');
+            this.fps = 60;
+            this.timer = 0;
+            this.interval = 1000/this.fps;
+        }
+        update(deltaTime){
+            if (this.frameX <= this.maxFrame) {
+                if (this.timer > this.interval){
+                    this.frameX++;
+                    this.timer = 0;
+                } else {
+                    this.timer += deltaTime;
+                }
+
+            }
+        }
+        draw(context){
+            context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.game.player.x, this.game.player.y, this.width, this.height);
+        }
+        reset(){
+            this.frameX = 0;
+            this.game.sound.shield();
+        }
     }
-    reset() {
-      this.frameX = 0;
-      this.game.sound.shield();
+    class Projectile {
+        constructor(game, x, y){
+            this.game = game;
+            this.x = x;
+            this.y = y;
+            this.width = 10;
+            this.height = 3;
+            this.speed = 3;
+            this.markedForDeletion = false;
+            this.image = document.getElementById('projectile');
+        }
+        update(){
+            this.x += this.speed;
+            if (this.x > this.game.width * 0.8) this.markedForDeletion = true;
+        }
+        draw(context){
+            context.drawImage(this.image, this.x, this.y);
+        }
     }
-  }
-  class Projectile {
-    constructor(game, x, y) {
-      this.game = game;
-      this.x = x;
-      this.y = y;
-      this.width = 10;
-      this.height = 3;
-      this.speed = 3;
-      this.markedForDeletion = false;
-      this.image = document.getElementById('projectile');
-    }
-    update() {
-      this.x += this.speed;
-      if (this.x > this.game.width * 0.8) this.markedForDeletion = true;
-    }
-    draw(context) {
-      context.drawImage(this.image, this.x, this.y);
-    }
-  }
-});
+    class Particle {
+        constructor(game, x, y){
+            this.game = game;
+            this.x = x;
+            this.y = y;
+            this.image = document.getElementById('gears');
+            this.frameX = Math.floor(Math.random() * 3);
+            this.frameY = Math.floor(Math.random() * 3);
+            this.spriteSize = 50;
+            this.sizeModifier = (Math.random() * 0.5 + 0.5).toFixed(1);
+            this.size = this.spriteSize * this.sizeModifier;
+            this.speedX = Math.random() * 6 - 3;
+            this.speedY = Math.random() * -15;
+            this.gravity = 0.5;
+            this.markedForDeletion = false;
+            this.angle = 0;
+            this.va = Math.random() * 0.2 - 0.1;
+            this.bounced = 0;
+            this.bottomBounceBoundary = Math.random() * 80 + 60;
+        }});
