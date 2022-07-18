c5564c5acc0c2f13a846be9f50b57d7c73847459
Linh Vu <linh777gkog@gmail.com>
2022-07-18 21:00:53 +0700

    creat input handler

--- script.js
+++ script.js
@@ -3,4 +3,26 @@ window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = 700;
-    canvas.height = 500;}
+    canvas.height = 500;
+
+    class InputHandler {
+        constructor(game){
+            this.game = game;
+            window.addEventListener('keydown', e => {
+                if ((   (e.key === 'ArrowUp') ||
+                        (e.key === 'ArrowDown')
+                ) && this.game.keys.indexOf(e.key) === -1){
+                    this.game.keys.push(e.key);
+                } else if ( e.key === ' '){
+                    this.game.player.shootTop();
+                } else if ( e.key === 'd'){
+                    this.game.debug = !this.game.debug;
+                }
+            });
+            window.addEventListener('keyup', e =>{
+                if (this.game.keys.indexOf(e.key) > -1){
+                    this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+                }
+            });
+        }
+    }
