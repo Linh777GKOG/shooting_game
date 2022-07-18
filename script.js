b8d2782256b0f0b32568035963b20cb5f4306b43
Linh Vu <linh777gkog@gmail.com>
2022-07-18 21:10:15 +0700

    update draw

--- script.js
+++ script.js
@@ -281,5 +281,32 @@ window.addEventListener('load', function () {
       this.frameY = 0;
       this.maxFrame = 37;
     }
+    update() {
+      this.x += this.speedX - this.game.speed;
+      if (this.x + this.width < 0) this.markedForDeletion = true;
+      // sprite animation
+      if (this.frameX < this.maxFrame) {
+        this.frameX++;
+      } else this.frameX = 0;
+    }
+    draw(context) {
+      if (this.game.debug)
+        context.strokeRect(this.x, this.y, this.width, this.height);
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
+      if (this.game.debug) {
+        context.font = '20px Helvetica';
+        context.fillText(this.lives, this.x, this.y);
+      }
+    }
   }
 });
